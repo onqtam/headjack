ac1941f7ebd056fc9701aca9f689aaa54700ad67
onqtam <vik.kirilov@gmail.com>
2023-10-04 17:41:00 +0300

    WIP

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,6 +68,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
+        hideOnScroll: true,
         title: 'Headjack',
         logo: {
           alt: 'Headjack Logo',
