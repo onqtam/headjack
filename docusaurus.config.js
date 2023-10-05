4ef6134bb7163c20369fbfc12aa7f39e14f438ac
onqtam <vik.kirilov@gmail.com>
2023-10-05 17:18:51 +0300

    WIP

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,9 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://headjack.online',
+  // url: 'https://headjack.online',
+  url: 'https://onqtam.github.io/headjack/',
+
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
