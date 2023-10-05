532d8819a0e4d30d02c9c69dbfd0f4e7d2ca6ddf
onqtam <vik.kirilov@gmail.com>
2023-10-05 17:09:56 +0300

    stuff

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'onqtam', // Usually your GitHub org/user name.
   projectName: 'headjack', // Usually your repo name.
+  trailingSlash: false,
 
   // onBrokenLinks: 'throw',
   onBrokenLinks: 'warn',
