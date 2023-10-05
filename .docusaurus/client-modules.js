69e66b4cc1f2f27c730afa4c8863661264d08c0f
onqtam <vik.kirilov@gmail.com>
2023-10-05 17:47:07 +0300

    adding files

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/onqtam/headjack/node_modules/infima/dist/css/default/default.css'),
+  require('/home/onqtam/headjack/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/onqtam/headjack/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/onqtam/headjack/src/css/custom.css'),
+];
